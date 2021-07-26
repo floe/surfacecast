2d0e1985838c46492729cbdc930df536fb1880b3
Florian Echtler <floe@butterbrot.org>
2021-07-26 22:02:10 +0200

    refactor client into own class/file

--- webrtc_server.py
+++ webrtc_server.py
@@ -9,114 +9,8 @@ gi.require_version('GstSdp', '1.0')
 from gi.repository import GLib, Gst, Soup, GstWebRTC, GstSdp
 
 from gst_helpers import *
+from client import *
 from webrtc_peer import WebRTCPeer
-
-clients = {}
-
-frontmixer  = None
-frontstream = None
-
-# links between individual client tee/mixer pairs
-mixer_links = []
-
-# position offsets for 4 front streams
-# FIXME: how to handle > 4 clients?
-offsets = [
-    (640,360),
-    (  0,  0),
-    (640,  0),
-    (  0,360)
-]
-
-class Client:
-
-    def __init__(self,name):
-
-        self.wrb = None
-        self.name = name
-        self.inputs  = {}
-        self.outputs = {}
-        self.mixers = {}
-
-    def create_mixers(self):
-
-        if "audio" in self.mixers or "surface" in self.mixers:
-            return
-
-        # setup surface & audio mixer
-        print("    creating mixers for client "+self.name)
-        self.mixers["surface"] = new_element("compositor",{"background":"black"},myname="mixer_"+self.name)
-        self.mixers["audio"]   = new_element("audiomixer",myname="audio_"+self.name)
-        add_and_link([self.mixers["surface"]])
-        add_and_link([self.mixers["audio"]])
-
-    # link client to frontmixer
-    def link_to_front(self):
-
-        # FIXME: frontstream is separately encoded for each client ATM, should be one single encoder
-        if not "front" in self.inputs or not "front" in self.outputs:
-            return
-
-        print("    linking client "+self.name+" to frontmixer")
-
-        # link frontstream tee to client-specific muxer
-        # TODO: seems to work witouth queue?
-        link_to_inputselector(frontstream,"src_%u",self.inputs["front"])
-
-        # request and link pads from tee and frontmixer
-        sinkpad = link_request_pads(self.outputs["front"],"src_%u",frontmixer,"sink_%u")
-        #sinkpad.set_property("max-last-buffer-repeat",10000000000) # apparently not needed
-
-        # set xpos/ypos properties on pad according to sequence number
-        # FIXME: only works with <= 4 clients at the moment
-        padnum = int(sinkpad.get_name().split("_")[1])
-        sinkpad.set_property("xpos",offsets[padnum][0])
-        sinkpad.set_property("ypos",offsets[padnum][1])
-
-    # helper function to link source tees to destination mixers
-    def link_streams_oneway(self,dest,prefix,qparams):
-
-        linkname = prefix+"_"+self.name+"_"+dest.name
-        if not linkname in mixer_links:
-
-            print("    linking client "+self.name+" to "+prefix+"mixer "+dest.name)
-            # TODO: needs a queue?
-            link_to_inputselector(self.outputs[prefix],"src_%u",dest.inputs[prefix])
-            #add_and_link([
-            #    self.outputs[prefix],
-            #    new_element("queue",qparams),
-            #    dest.mixers[prefix]
-            #])
-            mixer_links.append(linkname)
-
-            # for the "main" surface (identified through special SSRC),
-            # the destination mixer pad needs to have zorder = 0
-            # FIXME: feels like an ugly hack to do this here...
-            if prefix == "surface" and self.name == "FIXME":
-                print("    fixing zorder for main client")
-                sinkpads = dest.mixers[prefix].sinkpads
-                sinkpads[-1].set_property("zorder",0)
-
-    # link all other clients to this mixer, this client to other mixers
-    def link_streams(self,clients,prefix,qparams):
-
-        for c in clients:
-
-            if c == self.name: # skip own ssrc
-                continue
-
-            other = clients[c]
-
-            # for every _other_ mixer, link my tee to that mixer
-            self.link_streams_oneway(other,prefix,qparams)
-
-            # for every _other_ tee, link that tee to my mixer
-            other.link_streams_oneway(self,prefix,qparams)
-
-    # link all other clients to local mixer, this client to other mixers
-    def link_all_streams(self,clients):
-        self.link_streams(clients,"surface",{"max-size-buffers":1})
-        self.link_streams(clients,"audio",{"max-size-time":100000000})
 
 
 # incoming HTTP(S) request
@@ -145,79 +39,11 @@ def ws_conn_handler(server, connection, path, client, user_data):
     source = addr.get_address().to_string()+"_"+str(addr.get_port())
     print("New WebSocket connection from "+source)
 
-    clients[source] = Client(source)
+    add_new_client(source)
     wrb = WebRTCPeer(connection,source)
     connection.connect("closed",ws_close_handler,wrb)
-    clients[source].wrb = wrb
+    #clients[source].wrb = wrb
 
-# create single mixer for front stream
-def create_frontmixer_queue():
-
-    global frontmixer
-    global frontstream
-
-    if frontmixer != None or frontstream != None:
-        return
-
-    print("  creating frontmixer subqueue")
-
-    frontmixer  = new_element("compositor",myname="frontmixer")
-    frontstream = new_element("tee",{"allow-not-linked":True},myname="frontstream")
-
-    add_and_link([
-        frontmixer,
-        #new_element("videoconvert"),
-        # TODO: capsfilter here, probably
-        #new_element("queue",{"max-size-buffers":1}),
-        #new_element("x264enc",x264params),
-        frontstream
-    ])
-
-# link new client to mixers
-def link_new_client(client):
-
-    create_frontmixer_queue()
-
-    print("  setting up mixers for new client "+client.name)
-
-    # create surface/audio mixers for _all_ clients that don't have one yet
-    # needs to loop through all clients for the case where 2 or more clients
-    # appear simultaneously, otherwise there are no mixers to link to
-    # FIXME: will probably b0rk if one client is not yet completely initialized
-    if len(clients) >= 2:
-        for c in clients:
-            clients[c].create_mixers()
-
-    # add missing frontmixer links
-    client.link_to_front()
-
-    # add missing surface/audio mixer links
-    client.link_all_streams(clients)
-
-# new top-level element added to pipeline
-def on_element_added(thebin, element):
-
-    # check format: {input,output}_IPADDR_PORT_{surface,front,audio}
-    elname = element.get_name().split("_")
-    if len(elname) != 4:
-        return
-
-    direction = elname[0]
-    source = elname[1]+"_"+elname[2]
-    stype = elname[3]
-    #print(direction,source,stype)
-
-    client = clients[source]
-
-    if direction == "output":
-        client.outputs[stype] = element
-    if direction == "input":
-        client.inputs[stype] = element
-
-    # are all outputs in place?
-    if len(client.outputs) == 3:
-        print("Client elements complete.")
-        link_new_client(client)
 
 # "main"
 init_pipeline(on_element_added)
