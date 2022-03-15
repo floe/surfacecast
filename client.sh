abeedfa85b18060c591550b4d6fe2cf5a8db6f99
Florian Echtler <floe@butterbrot.org>
2022-03-15 15:30:17 +0100

    client wrapper script

---
+++ client.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+while true ; do
+	(sleep 10 && wmctrl -F -r surface -e 0,0,1100,-1,-1 && sleep 1 && wmctrl -F -r surface -b add,fullscreen && wmctrl -F -r front -b add,fullscreen) &
+	./webrtc_client.py -t butterbrot.org -f /dev/video-surf -s /dev/video20
+	clear
+	echo Restarting, please wait...
+	sleep $((2+$RANDOM/2000))
+done
