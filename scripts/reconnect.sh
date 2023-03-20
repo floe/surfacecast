34133f0fc8235c8bcb4c1ff04400bac02ac8b9b4
Florian Echtler <floe@butterbrot.org>
2023-03-20 10:03:46 +0100

    latency test script

--- scripts/reconnect.sh
+++ scripts/reconnect.sh
@@ -2,7 +2,7 @@
 i=1
 while true ; do
 	(sleep $((5+$RANDOM/2000)) && pkill webrtc_client) &
-	./webrtc_client.py --fake -t butterbrot.org -p 8081
+	./webrtc_client.py --fake -t butterbrot.org -p 8080
 	echo Restarting, please wait \(round $i\)...
 	sleep 5
 	i=$(($i+1))
