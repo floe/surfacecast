8a2271bf3a52069d3f236bd25f1c3ece4e9ffb03
Florian Echtler <floe@butterbrot.org>
2023-11-06 11:25:57 +0100

    helper scripts

--- scripts/multicast-sink.sh
+++ scripts/multicast-sink.sh
@@ -1,1 +1,2 @@
-gst-launch-1.0 udpsrc multicast-group=224.1.1.1 auto-multicast=true port=3000 ! application/x-rtp,media=video,cloc-rate=90000,encoding-name=H264,payload=96 ! rtph264depay ! avdec_h264 ! videoconvert ! fpsdisplaysink
+gst-launch-1.0 -tv udpsrc multicast-group=224.1.1.1 auto-multicast=true port=3000 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=H264,payload=96 ! rtph264depay ! avdec_h264 ! videoscale ! video/x-raw,width=1280,height=720 ! videoconvert ! video/x-raw,format=BGR ! v4l2sink device=/dev/video20
+#gst-launch-1.0 -tv udpsrc multicast-group=224.1.1.1 auto-multicast=true port=3000 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=H264,payload=96 ! rtph264depay ! avdec_h264 ! videoconvert ! fpsdisplaysink sync=false
