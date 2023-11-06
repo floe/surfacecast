59183faf580a9dc4c78d4db98b72fcfe79db452f
Florian Echtler <floe@butterbrot.org>
2023-11-06 13:49:04 +0100

    fix loopback handling

--- scripts/multicast-sink.sh
+++ scripts/multicast-sink.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export DISPLAY=":0.0"
 SINK="videoscale ! video/x-raw,width=1280,height=720 ! videoconvert ! video/x-raw,format=BGR ! v4l2sink device=/dev/video20"
-[ -e /dev/video20 ] || SINK="autovideosink" # fpsdisplaysink sync=false
+v4l2-ctl -D -k -d /dev/video20 | grep -q loopback || SINK="autovideosink" # fpsdisplaysink sync=false
 gst-launch-1.0 -tv udpsrc port=3000 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=H264,payload=96 ! rtph264depay ! avdec_h264 ! videoconvert ! $SINK
