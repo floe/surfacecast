f9e8ce2aa4d682062cd4e4b94877fc1660b5705d
Florian Echtler <floe@butterbrot.org>
2023-11-17 15:01:21 +0100

    minor webclient updates

--- webclient/tangram.js
+++ webclient/tangram.js
@@ -1,3 +1,9 @@
+// disable the canvas painting
+function onCanvasDown(evt) { }
+function onCanvasUp(evt) { }
+function onCanvasMove(evt) { }
+
+// directly add one sticker (= puzzle piece) each
 window.addEventListener("load", function(ev) {
 add_sticker(document.getElementById("t1"));
 add_sticker(document.getElementById("t2"));
