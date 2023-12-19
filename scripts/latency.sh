8b39bb53b38e9776bc3e1b72dafd2c7ac98b3070
Florian Echtler <floe@butterbrot.org>
2023-12-19 15:22:38 +0100

    enable headless latency test (closes #51)

--- scripts/latency.sh
+++ scripts/latency.sh
@@ -8,7 +8,7 @@ TARGET=localhost
 curl -k https://$TARGET:8080/quit
 sleep 2
 
-./webrtc_client.py --fake -t $TARGET -d -s "videotestsrc is-live=true pattern=ball ! timeoverlay ! debugqroverlay" &
-./webrtc_client.py --fake -t $TARGET -d --out "videoconvert ! zbar ! fpsdisplaysink sync=false" |& scripts/latency.py 0 &
+./webrtc_client.py --fake -t $TARGET -d --out "fakesink sync=false" -s "videotestsrc is-live=true pattern=ball ! timeoverlay ! debugqroverlay" &
+./webrtc_client.py --fake -t $TARGET -d --out "videoconvert ! zbar ! fakesink sync=false" |& scripts/latency.py 0 &
 sleep 60
 curl -k https://$TARGET:8080/quit
