ec662c6529c7d9b01da44b428bbce2a5b19ccadb
Florian Echtler <floe@butterbrot.org>
2024-01-16 14:22:28 +0100

    wayland/raspi support

--- scripts/multicast-sink.sh
+++ scripts/multicast-sink.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DISPLAY=":0.0"
-SINK="videoscale ! video/x-raw,width=1280,height=720 ! videoconvert ! video/x-raw,format=BGR ! v4l2sink device=/dev/video20"
-v4l2-ctl -D -k -d /dev/video20 | grep -q loopback || SINK="autovideosink" # fpsdisplaysink sync=false
-gst-launch-1.0 -tv udpsrc port=3000 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=H264,payload=96 ! rtph264depay ! avdec_h264 ! videoconvert ! $SINK
+#SINK="video/x-raw,format=BGR ! v4l2sink device=/dev/video20"
+SINK="fpsdisplaysink"
+v4l2-ctl -D -k -d /dev/video20 | grep -q loopback || SINK="waylandsink display=wayland-1 fullscreen=true"
+gst-launch-1.0 -mtv udpsrc address=0.0.0.0 port=3000 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=H264,payload=96 ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! videoconvert ! $SINK
