44c0be1fc9f512daddcff9d364151b5b366958e0
Florian Echtler <floe@butterbrot.org>
2024-02-14 17:45:55 +0100

    pulse instead of alsa

--- scripts/democlient2.sh
+++ scripts/democlient2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./webrtc_client.py -d -n Aalborg -t butterbrot.org --fake -a "alsasrc do-timestamp=true" \
+./webrtc_client.py -d -n Aalborg -t butterbrot.org --fake -a "pulsesrc do-timestamp=true" \
 	-f "v4l2src do-timestamp=true device=/dev/video0 ! image/jpeg ! jpegdec ! videorate ! videoconvert ! videocrop top=-1 bottom=-1 left=-1 right=-1" \
 	-s "udpsrc do-timestamp=true port=3000 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=H264,payload=96 ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! videoscale ! video/x-raw,width=1280,height=720 ! videoconvert" \
 	--persp "0.776637,-0.0148357,74.732,-0.0105345,0.783265,32.8821,-2.07266e-05,-3.60662e-05,0.996427" \
